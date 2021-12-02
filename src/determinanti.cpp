aa303a02b40e32811a3c95284fd46b887c6b9314
Simone Vigasio <vigasio.simone@gmail.com>
2021-12-02 14:54:52 +0100

    define det function

--- src/determinanti.cpp
+++ src/determinanti.cpp
@@ -9,36 +9,28 @@ float det(Matrix m) {
     if (m->nr == m->nc) {
         if (m->nr == 1) return m->mat[0][0];
         if (m->nr == 2) return m->mat[0][0]*m->mat[1][1]-m->mat[0][1]*m->mat[1][0];
-		int sub_row, sub_col;
+
         float determinante = 0.0;
+        // Definisco la sottomatrice sub_m (n-1)x(n-1)
 		Matrix sub_m = new Tmatrix(m->nr-1, m->nc-1); 
 		sub_m->mat = new float*[sub_m->nr];
-			for (int j=0; j<sub_m->nr; j++)
-				*(sub_m->mat + j) = new float[sub_m->nc];
+        for (int j=0; j<sub_m->nr; j++)
+            *(sub_m->mat + j) = new float[sub_m->nc];
+
+        // Riempio la sottomatrice sub_m sottraendo la riga i e la colonna 0 della matrice m
         for (int i=0; i<m->nr; i++) {
-			// non posso definire sub_m all'itero del ciclo
             for (int r=0; r<m->nr; r++) {
                 for (int c=1; c<m->nc; c++) {
-                    if (r != i) {
-						// condizione ? assegnamento1 : assegnamento2
-						if (r<i) {
-							sub_row=r;
-						} else {
-							sub_row=r-1;
-						}
-						sub_col=c-1;
-
-						sub_m->mat[sub_row][sub_col]=2.0;
-						sub_m->mat[sub_row][sub_col] = m->mat[r][c]; 
-
-                    }
+                    if (r != i) sub_m->mat[r<i ? r : r-1][c-1] = m->mat[r][c]; 
                 }
 			}
+
+            // Uso il teorema Laplace per determinare il determinante
             determinante += pow(-1.0, i)*m->mat[i][0]*det(sub_m);
         }
         return determinante;
     } else {
-        cout << endl << "Determinante non calcolabile";
+        cout << "You couldn't calculate determinant" << endl;
         return 0.0;
     }
 }
