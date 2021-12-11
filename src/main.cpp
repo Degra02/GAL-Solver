003939745f61933689f5aa7fd6922316e5908e64
Simone Vigasio <vigasio.simone@gmail.com>
2021-12-11 20:19:29 +0100

    .

--- src/main.cpp
+++ src/main.cpp
@@ -15,15 +15,11 @@ int main() {
 	// fraction
 	Fraction f1 = new Tfraction(-2, 1);
 	Fraction f2 = new Tfraction(500, 25);
-
-	simplification(f1)->print();
-	simplification(f2)->print();
-
-	sum(f1, f2)->print();	
-	difference(f1, f2)->print();
+	simplification(sum(f1, f2))->print();	
+	simplification(difference(f1, f2))->print();
 	simplification(product(f1, f2))->print();
 	simplification(quotient(f1, f2))->print();
-	power(f1, 3)->print();
+	simplification(power(f1, 3))->print();
 	
 	return 0;
 }
