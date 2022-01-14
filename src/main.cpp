d7554473de8d39ec8dd24275b4d2d941d3c51cf6
Simone Vigasio <vigasio.simone@gmail.com>
2022-01-14 12:16:53 +0100

    .

--- src/main.cpp
+++ src/main.cpp
@@ -12,17 +12,6 @@ using namespace std;
 
 int main() {
 	srand(time(0));
-<<<<<<< HEAD
-	Fraction f1 = new Tfraction(1.2);
-	Fraction f2 = new Tfraction(2.4);
-	fraction_simplification(f1)->print(); cout << endl;
-	fraction_simplification(f2)->print(); cout << endl;
-	fraction_sum(f1, f2)->print(); cout << endl;
-	fraction_difference(f1, f2)->print(); cout << endl;
-	fraction_product(f1, f2)->print(); cout << endl;
-	fraction_quotient(f1, f2)->print(); cout << endl;
-
-=======
 	Matrix m = new Tmatrix(3, 3, 1, 5);
 
 	cout << "Normal matrix" << endl;
@@ -40,7 +29,5 @@ int main() {
 	cout << endl << endl;
 	print_fract_matrix(m);
 	cout << endl << endl;
-	
->>>>>>> c7576a75d2f20060732c3e58634ee4d8fff61b13
 	return 0;
 }
