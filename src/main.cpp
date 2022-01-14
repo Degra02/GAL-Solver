89ba36677cfbd864ed18bbe13e7fc85a4172d53a
LivingButt <80328248+LivingButt@users.noreply.github.com>
2022-01-14 16:07:17 +0100

    Started working on the Fraction matrix

--- src/main.cpp
+++ src/main.cpp
@@ -4,30 +4,11 @@
 #include "all-headers.h"
 using namespace std;
 
-/* 
-	Main per fare test volta per volta
-	Sarà necessario creare delle procedure (void) per stampare un menù 
-	di opzioni che l'utente potrà scegliere
-*/
-
 int main() {
 	srand(time(0));
 
 	Matrix m = init_matrix();
 	print_matrix(m);
-
-	/*cout << "Vettore termini noti" << endl;
-	b->stampa();
-	cout << endl;
-	
-	cout << endl << "Sistema lineare" << endl;
-	Linear_System ls = new Tlisys(m, b);
-	Matrix m2 = to_matrix(ls);
-	m2->stampa();
-
-	cout << endl << "Riduzione a scalini" << endl;
-	m2 = ls_rref_full(ls);
-	m2->stampa();*/
 
 	return 0;
 }
