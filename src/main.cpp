ecf35ef82aede9e3dad46972ef47f5d06593432f
Simone Vigasio <vigasio.simone@gmail.com>
2022-01-17 16:17:33 +0100

    .

--- src/main.cpp
+++ src/main.cpp
@@ -6,14 +6,9 @@ using namespace std;
 
 int main() {
 	srand(time(0));
-	
-	FMatrix m = init_fmatrix("A");
-	print_fmatrix(m); print_fmatrix_float(m);
-	FMatrix mg = fraction_matrix_gauss_jordan(m);
-	print_fmatrix(mg); print_fmatrix_float(mg);
-	cout << "rango = " << fraction_matrix_rg(m) << endl;
-	FMatrix mr = fraction_matrix_rref(m);
-	print_fmatrix(mr); print_fmatrix_float(mr);
+	Lists list = new Tlists();
+	function_call(list);
 
+	
 	return 0;
 }
