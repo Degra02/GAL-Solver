a2e5d251d45594a2f2624026ba09133af6bd560a
LivingButt <80328248+LivingButt@users.noreply.github.com>
2022-01-22 11:00:07 +0100

    Removed obsolete files

--- include/all-headers.h
+++ include/all-headers.h
@@ -6,12 +6,6 @@
 #include <iomanip>
 #include <cstdlib>
 
-#include "matrix.h"
-#include "base.h"
-#include "determinant.h"
-#include "vector.h"
-#include "eq_systems.h"
-
 #include "fraction.h"
 #include "matrix_fraction.h"
 #include "vector_fraction.h"
