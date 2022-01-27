ccb6fd18fedb1972fe9cf8a7e0b9b0b0d5444ce5
Simone Vigasio <vigasio.simone@gmail.com>
2022-01-27 17:29:24 +0100

    format matrix fraction

--- include/fraction.h
+++ include/fraction.h
@@ -3,7 +3,7 @@
 #define __FRACTION_H__
 using namespace std;
 
-typedef struct Tfraction {
+typedef struct Tfraction{
     int num; int den;
 
     Tfraction();
@@ -13,23 +13,21 @@ typedef struct Tfraction {
     void print() const;
     void print_float() const;
     void set(float n); 
-} Tfraction;
+}Tfraction;
 
 typedef Tfraction* Fraction;
 
-/* operazioni base delle frazioni */
 Fraction fraction_power(Fraction a, int p);
 Fraction fraction_sum(Fraction a, Fraction b);
 Fraction fraction_difference(Fraction a, Fraction b);
 Fraction fraction_product(Fraction a, Fraction b);
 Fraction fraction_quotient(Fraction a, Fraction b);
 Fraction fraction_simplification(Fraction a);
-
+Fraction fraction_copy(Fraction a);
 Fraction str_to_fraction(string value);
 void print_format_fraction(Fraction f, int max_figures_num, int max_figures_den);
 void zero_control(Fraction f);
 void sign_control(Fraction f);
 void control(Fraction f);
-Fraction fraction_copy(Fraction a);
 
 #endif
